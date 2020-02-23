198ee414a74dc21500279957d917916de0d25a3b
Roberto Estrada <restrada@paridin.com>
2020-02-22 23:41:07 -0600

    update patch version

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule GenTemplateDinoLiveView.Mixfile do
   use Mix.Project
 
   @name    :gen_template_dino_live_view
-  @version "0.1.5"
+  @version "0.1.6"
 
   @deps [
     { :mix_templates,  ">0.0.0",  app: false },
