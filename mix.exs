9144a7a0bf6eac0ff245b8a5b64c964b2dc0efc7
Roberto Estrada <restrada@paridin.com>
2020-07-22 15:59:07 -0500

    addes formatter

--- mix.exs
+++ mix.exs
@@ -1,16 +1,16 @@
 defmodule GenTemplateDinoLiveView.Mixfile do
   use Mix.Project
 
-  @name    :gen_template_dino_live_view
+  @name :gen_template_dino_live_view
   @version "0.2.11"
 
   @deps [
-    { :mix_templates,  ">0.0.0",  app: false },
-    { :ex_doc,         ">0.0.0",  only: [:dev, :test] },
+    {:mix_templates, ">0.0.0", app: false},
+    {:ex_doc, ">0.0.0", only: [:dev, :test]}
   ]
 
   @maintainers ["Roberto Estrada <dev@paridin.com>"]
-  @github      "https://github.com/paridin/#{@name}"
+  @github "https://github.com/paridin/#{@name}"
 
   @description """
   An opinionated template to create phoenix apps, using live view and tailwind. (Live Dashboard Supported)
@@ -19,29 +19,29 @@ defmodule GenTemplateDinoLiveView.Mixfile do
   # ------------------------------------------------------------
 
   def project do
-    in_production = Mix.env == :prod
+    in_production = Mix.env() == :prod
+
     [
-      app:     @name,
+      app: @name,
       version: @version,
-      deps:    @deps,
-      elixir:  "~> 1.9",
+      deps: @deps,
+      elixir: "~> 1.9",
       package: package(),
-      description:     @description,
-      build_embedded:  in_production,
-      start_permanent: in_production,
+      description: @description,
+      build_embedded: in_production,
+      start_permanent: in_production
     ]
   end
 
   defp package do
     [
-      name:        @name,
-      files:       ["lib", "mix.exs", "README.md", "LICENSE.md", "template", "base"],
+      name: @name,
+      files: ["lib", "mix.exs", "README.md", "LICENSE.md", "template", "base"],
       maintainers: @maintainers,
-      licenses:    ["Apache 2.0"],
-      links:       %{
-        "GitHub" => @github,
+      licenses: ["Apache 2.0"],
+      links: %{
+        "GitHub" => @github
       }
     ]
   end
-
 end
