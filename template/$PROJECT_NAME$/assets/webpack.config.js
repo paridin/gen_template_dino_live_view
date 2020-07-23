12c48229073c4e5f989b57145a633bbee379008a
Roberto Estrada <restrada@paridin.com>
2020-07-22 19:06:45 -0500

    fix issue with copy-webpack-plugin > 6.0.0

--- template/$PROJECT_NAME$/assets/webpack.config.js
+++ template/$PROJECT_NAME$/assets/webpack.config.js
@@ -1,25 +1,25 @@
-const path = require('path');
-const glob = require('glob');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const glob = require("glob");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = (env, options) => {
-  const devMode = options.mode !== 'production';
+  const devMode = options.mode !== "production";
   return {
     optimization: {
       minimizer: [
         new TerserPlugin({ cache: true, parallel: true, sourceMap: false }),
-        new OptimizeCSSAssetsPlugin({})
-      ]
+        new OptimizeCSSAssetsPlugin({}),
+      ],
     },
     entry: {
-      './js/app.js': glob.sync('./vendor/**/*.js').concat(['./js/app.js'])
+      "./js/app.js": glob.sync("./vendor/**/*.js").concat(["./js/app.js"]),
     },
     output: {
-      filename: 'app.js',
-      path: path.resolve(__dirname, '../priv/static/js')
+      filename: "app.js",
+      path: path.resolve(__dirname, "../priv/static/js"),
     },
     module: {
       rules: [
@@ -27,32 +27,32 @@ module.exports = (env, options) => {
           test: /\.js$/,
           exclude: /node_modules/,
           use: {
-            loader: 'babel-loader'
-          }
+            loader: "babel-loader",
+          },
         },
         {
           test: /\.css$/,
           use: [
             MiniCssExtractPlugin.loader,
-            'css-loader',
+            "css-loader",
             {
-              loader: 'postcss-loader',
+              loader: "postcss-loader",
               options: {
-                ident: 'postcss',
+                ident: "postcss",
                 plugins: [
-                  require('postcss-import'),
-                  require('tailwindcss')('./tailwind.config.js'),
-                  require('postcss-preset-env')({ stage: 1 }),
-                ]
-              }
-            }
-          ]
-        }
-      ]
+                  require("postcss-import"),
+                  require("tailwindcss")("./tailwind.config.js"),
+                  require("postcss-preset-env")({ stage: 1 }),
+                ],
+              },
+            },
+          ],
+        },
+      ],
     },
     plugins: [
-      new MiniCssExtractPlugin({ filename: '../css/app.css' }),
-      new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
-    ]
-  }
+      new MiniCssExtractPlugin({ filename: "../css/app.css" }),
+      new CopyWebpackPlugin({ patterns: [{ from: "static/", to: "../" }] }),
+    ],
+  };
 };
