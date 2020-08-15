770f96fc5f6c7f7b75b01ab4758b1a152de54918
Roberto Estrada <restrada@paridin.com>
2020-08-15 16:26:26 -0500

    update to whitelist info messages

--- template/$PROJECT_NAME$/assets/tailwind.config.js
+++ template/$PROJECT_NAME$/assets/tailwind.config.js
@@ -10,6 +10,31 @@ module.exports = {
       "./js/**/*.js",
     ],
     defaultExtractor: (content) => content.match(/[\w-/.:]+(?<!:)/g) || [],
+    options: {
+      whitelist: [
+        // required by info messages
+        "bg-blue-50",
+        "bg-blue-100",
+        "text-blue-400",
+        "text-blue-500",
+        "text-blue-800",
+        "bg-yellow-50",
+        "bg-yellow-100",
+        "text-yellow-400",
+        "text-yellow-500",
+        "text-yellow-800",
+        "bg-red-50",
+        "bg-red-100",
+        "text-red-400",
+        "text-red-500",
+        "text-red-800",
+        "bg-green-50",
+        "bg-green-100",
+        "text-green-400",
+        "text-green-500",
+        "text-green-800",
+      ],
+    },
   },
   theme: {
     fontFamily: {
