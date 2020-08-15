82f13654818028e0013b15f5a825d2ad53227001
Roberto Estrada <restrada@paridin.com>
2020-08-15 16:26:03 -0500

    fixed issues with optimizecssassetsplugins

--- template/$PROJECT_NAME$/assets/webpack.config.js
+++ template/$PROJECT_NAME$/assets/webpack.config.js
@@ -2,7 +2,6 @@ const path = require("path");
 const glob = require("glob");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
-const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = (env, options) => {
@@ -11,7 +10,6 @@ module.exports = (env, options) => {
     optimization: {
       minimizer: [
         new TerserPlugin({ cache: true, parallel: true, sourceMap: false }),
-        new OptimizeCSSAssetsPlugin({}),
       ],
     },
     entry: {
