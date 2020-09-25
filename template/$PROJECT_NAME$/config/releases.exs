07a08ba9d4cfcd00f522857f45d0a9332fcd3ce1
Roberto Estrada <restrada@paridin.com>
2020-09-25 12:18:00 -0500

    improve configuration to handle basis

--- template/$PROJECT_NAME$/config/releases.exs
+++ template/$PROJECT_NAME$/config/releases.exs
@@ -9,6 +9,23 @@ secret_key_base =
     You can generate one by calling: mix phx.gen.secret
     """
 
+port = System.get_env("HTTP_PORT") || "80"
+
+schema = case port do
+  "443" -> "https"
+  "80" -> "http"
+  _ -> System.get_env("HTTP_SCHEMA") || raise """
+  environment variable HTTP_SCHEMA is required since we can't get it from bases protocol.
+  """
+end
+
 config :<%= @project_name %>, <%= @project_name_camel_case %>Web.Endpoint,
-  http: [:inet6, port: String.to_integer(System.get_env("PORT") || "80")],
+  url: [
+    schema: schema,
+    host: System.get_env("HTTP_HOST") || "localhost",
+    port: String.to_integer(System.get_env("URL_PORT") || port)
+  ],
+  http: [:inet6, port: String.to_integer(port)],
+  # if you need configure https here is the place.
+  check_origin: (System.get_env("HTTP_ORIGINS") || "localhost") |> String.split(" "),
   secret_key_base: secret_key_base
