6d0d0312ab119f1293930a4abf3d564833175ff1
Roberto Estrada <restrada@paridin.com>
2020-11-22 21:39:35 -0600

    0.2.19

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule GenTemplateDinoLiveView.Mixfile do
   use Mix.Project
 
   @name :gen_template_dino_live_view
-  @version "0.2.18"
+  @version "0.2.19"
 
   @deps [
     {:mix_templates, ">0.0.0", app: false},
